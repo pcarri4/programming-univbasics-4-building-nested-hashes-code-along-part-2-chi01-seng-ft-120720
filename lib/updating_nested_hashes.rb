82dcd4cd36f337f35bb40ac68fbfd8cd8889b97e
Paulina Carrillo <paulinacarri4@gmail.com>
2020-11-27 23:07:54 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -46,8 +46,6 @@ def add_characters
       }
    }
 
- epic_tragedy[:additional_characters] = [
-   {name: "Prince Escalus"}, 
-   {name: "Apothecary"}
-   ]
+ epic_tragedy[:additional_characters] << {name: "Prince Escalus"}
+ epic_tragedy[:additional_characters] << {name: "Apothecary"}
 end
